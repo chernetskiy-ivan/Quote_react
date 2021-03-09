e7ea5b5f87bde26dc74c7d912ac6dc245477dff0
Ivan <ivanchernetskey@mail.ru>
2021-03-09 22:42:49 +0300

    Yeah it fucking work. I use useState for change title, body due to async requests

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Card() {
+
+    const [count, setCount] = useState(1)
+    const [title,setTitle] = useState('Привет')
+    const [body, setBody] = useState('Привет')
+
+    const increment = () => {
+        setCount( count + 1)
+        console.log(count)
+        getContent(count)
+    }
+
+    function getContent(number){
+        fetch(`https://jsonplaceholder.typicode.com/posts?id=${number}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach( element => {
+                    setTitle(element.title)
+                    setBody(element.body)
+                    console.log(element)
+                    console.log(element.title)
+                })
+            })
+    }
+
     return (
         <div className='card'>
-            <div className='title'>title</div>
-            <div className='quote'>Quote here!</div>
+            <div className='title'>{title}</div>
+            <div className='quote'>{body}</div>
             <div className='footer'>
                 <div className='btn btn-back'>Назад</div>
-                <div className='btn btn-ahead'>Вперед</div>
+                <div className='btn btn-ahead' onClick={increment}>Вперед</div>
             </div>
         </div>
     )
